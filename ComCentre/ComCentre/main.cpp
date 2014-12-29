75bcdac2418605f017319e5e4500fb5947770952
Wojtechnology <wswiders@uwaterloo.ca>
2014-12-29 16:23:57 -0500

    Goodbye Google

--- ComCentre/ComCentre/main.cpp
+++ ComCentre/ComCentre/main.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::cout << "Goodbye Google!\n";
     return 0;
 }
