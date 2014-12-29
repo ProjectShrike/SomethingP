115b91148496d4a814ae8c0086086cea039e8a76
Wojtechnology <wswiders@uwaterloo.ca>
2014-12-29 16:19:55 -0500

    Create project

---
+++ ComCentre/ComCentre/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ComCentre
+//
+//  Created by Wojtek Swiderski on 2014-12-29.
+//  Copyright (c) 2014 ProjectShrike. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
